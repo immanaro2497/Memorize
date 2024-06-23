5bcb7e8bdc3d5390674d110742713e5c90d6cb8d
immanaro2497 <immanuel2497@gmail.com>
2024-06-23 15:43:47 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Immanuel on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
